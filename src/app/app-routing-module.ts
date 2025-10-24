977a415a6248789054e4f628435f8b3a42c26b70
marquigi <markluigi3@yahoo.it>
2025-10-25 00:13:38 +0200

    .

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -8,22 +8,31 @@ import { ValidatoriCustomControlComponent } from './components/validatori-custom
 import { ValidatoriCustomGroupComponent } from './components/validatori-custom-group.component/validatori-custom-group.component';
 
 const routes: Routes = [
+  // Configurazione delle rotte principali dell'app
   { path: '', component: FormControlComponent },
+  // Rotta base: mostra FormControlComponent
   { path: 'form-group', component: FormGroupComponent },
+  // Rotta per esempio FormGroup
+  // Sezione dedicata ai validatori con rotte annidate
   {
     path: 'validatori',
     component: ValidatoriComponent,
     children: [
       { path: '', component: GestioneErroriComponent },
+      // Rotta iniziale dei validatori: gestione errori
       { path: 'custom-control', component: ValidatoriCustomControlComponent },
+      // Esempio validatore custom su FormControl
       { path: 'custom-group', component: ValidatoriCustomGroupComponent },
+      // Esempio validatore custom su FormGroup
     ],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  // Inizializzo il router con queste rotte
   exports: [RouterModule],
+  // Esporto il Router per usarlo in tutta l'app
 })
 export class AppRoutingModule { }
 
